73ea8687ec7535b7e470e1b4b801f04db8f74a82
mintmarshmallow <nopp054@naver.com>
2019-01-05 19:46:07 +0900

    Add a intro popup

--- intro.js
+++ intro.js
@@ -1,13 +1,16 @@
 window.onload = function() {
-
     const introMusic = document.getElementById('introMusic');
     const startRewindBtn = document.getElementById('startRewindBtn');
     const container = document.getElementById('container');
+    const introPopUp = document.getElementById('introPopUp');
+    const projectInfo = document.getElementById('projectInfo');
+    const xIcon = document.getElementById('xIcon')
     startRewindBtn.onclick = startRewind;
     let prevScroll = 0;
+    introMusic.volume = 0.5;
     window.addEventListener('scroll', function() {
         //let volumeDecrement = 0.01;
-        let volumeDecrement = window.scrollY/1200;
+        let volumeDecrement = window.scrollY/3000;
         /*if(window.scrollY > prevScroll && (introMusic.volume-volumeDecrement) >= 0){
           setTimeout(function() {
             introMusic.volume = introMusic.volume-volumeDecrement
@@ -20,8 +23,9 @@ window.onload = function() {
           }, 500);
         }*/
 
-          if(window.scrollY !== 0 && 1-(volumeDecrement) >= 0 && 1-(volumeDecrement) <= 1){
-            introMusic.volume = 1-(volumeDecrement);
+          if(window.scrollY !== 0 && 0.5-(volumeDecrement) >= 0 && 0.5-(volumeDecrement) <= 1){
+
+            introMusic.volume = 0.5-(volumeDecrement);
           }
         //prevScroll = window.scrollY
     })
@@ -30,6 +34,10 @@ window.onload = function() {
         introMusic.play();
         startRewindBtn.classList.add('unvisible');
         container.classList.remove('unvisible');
+        introPopUp.classList.remove('unvisible')
+        introPopUp.classList.add('visible');
+        projectInfo.classList.remove('unvisible');
+        xIcon.classList.remove('unvisible')
     }
 
 }
